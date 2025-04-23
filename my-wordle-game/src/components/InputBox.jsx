720e533044f8446505cc8e8fa37c6cb3525abfa7
mkpouto-inyang <mkay.inyang31@gmail.com>
2025-04-24 00:13:20 +0100

    capitalize input function works

--- my-wordle-game/src/components/InputBox.jsx
+++ my-wordle-game/src/components/InputBox.jsx
@@ -1,14 +1,21 @@
-const InputBox = () => {
+import { useState } from "react"
 
-    const capitalizeInput = (e) =>{
+
+const InputBox = () => {
+    const [inputLetter, setInputLetter] = useState("")
+
+    const capitalizeInput = (e) => {
         const letter = e.target.value
         console.log(letter)
+        const capitalizedLetter = letter.toUpperCase()
+        setInputLetter(capitalizedLetter)
     }
     return ( 
         <>
             <input 
             className="w-[55px] h-[55px] border-[#d4d1d1] border rounded-[4px] text-center mr-2 font-bold text-[30px]" 
-            maxLength={1}
+            value={inputLetter}
+            maxLength={2}
             onChange={capitalizeInput}>
             </input>
         </>
