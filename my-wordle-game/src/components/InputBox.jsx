f797750ca3b26e56ece7f2bb711adee3ebd0c571
mkpouto-inyang <mkay.inyang31@gmail.com>
2025-04-24 01:24:07 +0100

    fixed issue of event being undefined

--- my-wordle-game/src/components/InputBox.jsx
+++ my-wordle-game/src/components/InputBox.jsx
@@ -4,19 +4,38 @@ import { useState } from "react"
 const InputBox = () => {
     const [inputLetter, setInputLetter] = useState("")
 
+    const wordLength = 5;
+    let wordEntry = "";
+
+    const handleNextInputFocus = () =>{
+        console.log('NEXTINPUTFOCUS FUNCTION')
+    }
+
+    const logWordEntry = () =>{
+        console.log('LOGWORDENTRY')
+
+    }
+
     const capitalizeInput = (e) => {
         const letter = e.target.value
-        console.log(letter)
         const capitalizedLetter = letter.toUpperCase()
         setInputLetter(capitalizedLetter)
+        console.log(e)
     }
+
+    const handleChange = (e) =>{
+        capitalizeInput(e);
+        handleNextInputFocus();
+        logWordEntry()
+    }
+
     return ( 
         <>
             <input 
             className="w-[55px] h-[55px] border-[#d4d1d1] border rounded-[4px] text-center mr-2 font-bold text-[30px]" 
             value={inputLetter}
             maxLength={2}
-            onChange={capitalizeInput}>
+            onChange={handleChange}>
             </input>
         </>
      );
